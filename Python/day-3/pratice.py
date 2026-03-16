76027548239e4fa222cd03aaa68f3b1aac03b5fe
Keerthi Kumar V <kumarkeerthi442@gmail.com>
2026-03-16 06:38:45 +0530

    day-3 done

---
+++ Python/day-3/pratice.py
@@ -0,0 +1,35 @@
+text = "Python"
+print(text[1:4])
+
+text = "Python"
+print(text[-1])
+
+text = "Programming"
+print(text[3:7])
+
+text = "python"
+print(text.upper())
+
+text = "PYTHON"
+print(text.lower())
+
+python = "Python Programming"
+print (python)
+
+a = "Python"
+print(a[:4])
+
+b= "HELLO"
+print(b.lower())
+
+c="I love Java"
+print(c.replace("Java", "Python"))
+
+txt = "Hello, World!"
+print(txt[6:])
+
+text = "Python"
+print(len(text))
+
+text = "Hello,Python"
+print(text.split(","))
