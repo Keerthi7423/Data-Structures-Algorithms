ae043f02df6bc487d2a4b0f36e95d566c5a32767
Keerthi Kumar V <kumarkeerthi442@gmail.com>
2026-03-22 19:29:59 +0530

    python day-6

---
+++ Python/day-6/day-6.py
@@ -0,0 +1,40 @@
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+print(bool("Hello"))
+print(bool(15))
+
+print(bool("abc"))
+print(bool(123))
+print(bool(["apple", "cherry", "banana"]))
+
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool(()))
+print(bool([]))
+print(bool({}))
+
+def myFunction() :
+      return True
+
+print(myFunction())
+
+def myFunction() :
+      return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
---
+++ Python/day-6/pratice.py
@@ -0,0 +1,64 @@
+print(bool(1))
+
+print(bool(0))
+
+print(bool(""))
+
+print(bool("Hello"))
+
+x = 5
+print(x > 3)
+
+x = 5
+print(x < 3)
+
+x = 10
+y = 10
+
+print(x == y)
+
+x = 10
+y = 5
+
+print(x != y)
+
+x=12
+print(x >10)
+
+x=1
+print(x == 0)
+
+x=100
+print(x == 100)
+
+x = 23
+
+
+# # Take input from user
+# num = int(input("Enter a number: "))
+
+# # Check condition
+# if 1 <= num <= 50:
+#     print("The number is between 1 and 50 (inclusive).")
+# else:
+#     print("The number is NOT between 1 and 50.")
+    
+
+# number = int(input("Enter a number:"))
+
+# if 1 <= number <= 10:
+#     print("The number is between 1 and 10 ")
+# else:
+#     print("The number is not between 1 and 10")
+
+print(bool([]))
+
+print(bool([1, 2, 3]))
+
+print(bool(None))
+
+print(5 > 3 and 10 > 5)
+
+print(5 > 3 and 10 < 5)
+
+print(5 > 3 or 10 < 5)
