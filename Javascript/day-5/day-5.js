ca7f5921905405278e204ea9ba4bf2a2db2028c6
Keerthi Kumar V <kumarkeerthi442@gmail.com>
2026-03-24 23:09:38 +0530

    js/day-5

---
+++ Javascript/day-5/day-5.js
@@ -0,0 +1,31 @@
+let x = 5;
+x++;
+console.log(x); // 6
+
+let y = 5;
+y--;
+console.log(y); 
+
+let x1 = 5;
+let z = x1 ** 2;
+console.log(z);
+
+
+// Day 5 - Operators
+
+let a = 10;
+let b = 5;
+
+// Arithmetic
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+// Comparison
+console.log(a > b);
+console.log(a === b);
+
+// Logical
+console.log(a > b && b > 2);
+console.log(a < b || b > 2);
