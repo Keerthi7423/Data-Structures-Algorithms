5ddce2d308ab28e17003bad3c35184767f452082
Keerthi Kumar V <kumarkeerthi442@gmail.com>
2026-03-24 23:00:32 +0530

    day-4

---
+++ Javascript/day-4/day-4.js
@@ -0,0 +1,24 @@
+let name = "Keerthi";  // String
+console.log(name);
+
+let age = 22;
+let price = 99.99;
+
+let isActive = true;
+
+let person = {
+  name: "Keerthi",
+  age: 22
+};
+
+let numbers = [1, 2, 3, 4];
+
+console.log(age);
+console.log(price);
+console.log(isActive);
+console.log(person);
+console.log(numbers);
+
+console.log(typeof "Hello"); // string
+console.log(typeof 100);     // number
+console.log(typeof true);    // boolean
