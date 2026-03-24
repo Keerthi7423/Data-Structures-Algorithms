2855df6f712f2abc7e73d71a0c85187104c7586d
Keerthi Kumar V <kumarkeerthi442@gmail.com>
2026-03-24 06:36:43 +0530

    day-3

---
+++ Javascript/day-3/pratice.js
@@ -0,0 +1,41 @@
+// let x = 10;
+// x = 20;
+// console.log(x);
+// const x = 10;
+// x = 20;
+// console.log(x);
+// var x = 10;
+// var x = 20;
+// console.log(x);
+// let x;
+// console.log(x);
+
+// console.log(typeof "Hello");
+
+// console.log(typeof null);
+
+// let name = "Keerthi";
+// console.log(name);
+
+// const birthyear = 2002;
+// console.log(birthyear);
+
+// let name = "Keerthi";
+// let age = 22;
+// let isStudent = true;
+
+// var x = "Keerthi";
+// let y = 20;
+// const z = true;
+
+// console.log(x);
+// console.log(y);
+// console.log(z);
+
+var x = "Keerthi";
+let y = 20;
+const z = true;
+
+console.log( typeof x);
+console.log(typeof y);
+console.log( typeof z);
