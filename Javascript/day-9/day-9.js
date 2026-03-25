9b434399162fafe1b02e431c4c3ff32b77d59665
Keerthi Kumar V <kumarkeerthi442@gmail.com>
2026-03-25 22:28:02 +0530

    day-9

---
+++ Javascript/day-9/day-9.js
@@ -0,0 +1,22 @@
+// Day 9 - String Methods
+
+let text = "  JavaScript Learning  ";
+
+// Trim
+let cleanText = text.trim();
+
+// Upper & Lower
+console.log(cleanText.toUpperCase());
+console.log(cleanText.toLowerCase());
+
+// Index
+console.log(cleanText.indexOf("Script"));
+
+// Replace
+console.log(cleanText.replace("Learning", "Practice"));
+
+// Slice
+console.log(cleanText.slice(0, 10));
+
+// Includes
+console.log(cleanText.includes("Java"));
---
+++ Javascript/day-9/pratice.js
@@ -0,0 +1,32 @@
+// let str = "hello";
+// console.log(str.toUpperCase());
+
+// let str1 = "HELLO";
+// console.log(str1.toLowerCase());
+
+// let str2 = "  hello  ";
+// console.log(str2.trim());
+
+// let str3 = "JavaScript";
+// console.log(str3.includes("Script"));
+
+// let str4 = "JavaScript";
+// console.log(str4.indexOf("a"));
+
+// let str5 = "JavaScript";
+// console.log(str5.slice(0, 4));
+
+// let str6 = "JavaScript";
+// console.log(str6.slice(4));
+
+// let str7 = "Hello World";
+// console.log(str7.replace("World", "JS"));
+
+// let str = "hello";
+// console.log(str[0].toUpperCase() + str.slice(1));
+
+let str = "JavaScript";
+console.log(str.slice(-6));
+
+let str1 = "  JS  ";
+console.log(str1.trim().length);
