e8cb6c776254ea5a5ea278ded65948f9a40b8e81
Keerthi Kumar V <kumarkeerthi442@gmail.com>
2026-03-28 16:11:45 +0530

    js/day-10

---
+++ Javascript/day-10/day-10.js
@@ -0,0 +1,20 @@
+// OLd way of concatenating strings
+let name = "Keerthi";
+console.log("Hello" + name);
+
+let name1 = "King Cobra";
+console.log(`The name of my alternative is:${name1}`);
+
+//Expression Inside Template Literals
+let a = 10;
+let b =20;
+console.log(`The sum of a and b is: ${a + b}`);
+
+console.log(`This is
+multi-line
+string`);
+
+let name2 = "Keerthi";
+let age = 22;
+
+console.log(`Name: ${name2}, Age: ${age}`);
