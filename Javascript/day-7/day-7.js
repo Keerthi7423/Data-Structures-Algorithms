0ea1e1fee7625831c2b3a02dc980910c868d8438
Keerthi Kumar V <kumarkeerthi442@gmail.com>
2026-03-28 15:03:38 +0530

    day-7

---
+++ Javascript/day-7/day-7.js
@@ -0,0 +1,30 @@
+let number = "10";
+if (number === 10){
+    console.log("Number is 10");
+} {
+        console.log("Number is not 10")
+    }
+
+
+    let num1 = 10;
+
+    if( num1 % 2) {
+        console.log("number is odd");
+    } else{
+        console.log("number is even");
+    }
+
+    let num2 = 15;
+    num2 = 20;
+    
+    console.log(num2);
+
+
+    let celsius = 25;
+    let fahrenheit = (celsius * 9/5) + 32;
+    console.log("Temperature in Fahrenheit: " + fahrenheit);
+
+    console.log([] + []);
+
+    console.log([] + {});
+console.log({} + []);
