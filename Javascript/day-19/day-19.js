e9a89403c0633a36c8eeeea800e1120caa77e730
Keerthi Kumar V <kumarkeerthi442@gmail.com>
2026-04-13 05:52:11 +0530

    arrays

---
+++ Javascript/day-19/day-19.js
@@ -0,0 +1,18 @@
+// // 1️⃣ Create an array of 5 numbers and print it
+//  let numbers = [10, 20, 30, 40, 50];
+//     console.log(numbers);
+
+// // 2️⃣ Print the first element of an array
+// console.log(numbers[0]);
+
+// // 3️⃣ Print the last element of an array
+
+// console.log(numbers[numbers.length - 1]);
+// // 4️⃣ Update the second element of an array
+
+// numbers[1] = 25;
+// console.log(numbers);
+// // 5️⃣ Replace last element with a new value
+// numbers[numbers.length - 1] = 60;
+// console.log(numbers);
+
