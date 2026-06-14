9283edd339fac2d014438f1c74b99f6ef5ea5201
Keerthi Kumar V <kumarkeerthi442@gmail.com>
2026-06-14 19:02:54 +0530

    basics of day-1

---
+++ C++/day-1/basics.cpp
@@ -0,0 +1,12 @@
+// #include<iostream>
+// int main() {
+//     std::cout << "Hey, Striver!";
+//     return 0;
+// }
+
+#include<iostream>
+int main() {
+    std::cout << "Hey, Striver!" << "\n";
+    std::cout << "Hey, Striver!";
+    return 0;
+}
