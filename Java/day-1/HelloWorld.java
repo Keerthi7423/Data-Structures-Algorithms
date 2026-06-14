91837cfe271cf62c9626701529624ad245ab07e3
Keerthi Kumar V <kumarkeerthi442@gmail.com>
2026-06-14 19:41:36 +0530

    Day 01 - Java Fundamentals

---
+++ Java/day-1/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+            String name = "Keerthi Kumar V";
+            int age = 23;
+            String city = "Bengaluru";
+            System.out.println("Hello, my name is " + name + ". I am " + age + " years old and I live in " + city + ".");
+    }
+}
