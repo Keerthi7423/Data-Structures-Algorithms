3f20703903520fcc1c83cb2870a94c552d2d4b8a
Keerthi Kumar V <kumarkeerthi442@gmail.com>
2026-06-20 18:48:31 +0530

    java basics

---
+++ Java/day-1/Program2.java
@@ -0,0 +1,9 @@
+public class Program2 {
+    public static void main(String[] args) {
+        String name = "Keerthi";
+        int age = 24;
+        int salary = 50000;
+        System.out.println("Hello, " + name + "! You are " + age + " years old and your salary is $" + salary + ".");
+    }
+}
+   
