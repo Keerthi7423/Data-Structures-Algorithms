f2003dc8129f3cc1a8fffe122651e00e43b0dc14
Keerthi Kumar V <kumarkeerthi442@gmail.com>
2026-07-14 06:51:20 +0530

    merge sorted arays

---
+++ Javascript/day-78/BruteForce.js
@@ -0,0 +1,27 @@
+function merge(nums1,m,nums2,n){
+
+    for(let i=0;i<n;i++){
+
+        nums1[m+i]=nums2[i];
+
+    }
+
+    nums1.sort((a,b)=>a-b);
+
+    return nums1;
+
+}
+
+// Test Cases
+
+console.log(
+merge([1,2,3,0,0,0],3,[2,5,6],3)
+);
+
+console.log(
+merge([1],1,[],0)
+);
+
+console.log(
+merge([0],0,[1],1)
+);
